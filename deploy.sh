kayushkin/argraphments

deploy.sh: refuse to install a binary with no VCS stamp
go build writes no VCS information when it cannot find a .git DIRECTORY, and it
does not fail when that happens -- not even with -buildvcs=true, and not even
with no repository at all (measured on go1.26.0). A git worktree's .git is a
pointer file, so a deploy run from one installs a binary stamped (devel) with no
vcs.revision, while every test and smoke passes and the log reads clean.

A provenance stamp is the one property whose absence looks exactly like success,
so assert it rather than trusting the build. The check runs before the install,
for the same reason the smokes do: installing first would put an untraceable
binary in front of live sessions and only then report it.

A dirty tree (vcs.modified=true) defeats provenance the same way -- the recorded
revision names the commit the binary was built NEAR, not the source it was built
FROM. That warns rather than refuses, because most of the Go binaries currently
in ~/bin are in that state and refusing would be a behaviour change the user has
not been asked about yet.

deploy.sh
@@ -61,6 +61,28 @@ echo "==> Building $BINARY_NAME..."
 go build -o "$BINARY_NAME" .
 echo "    built: $(ls -lh "$BINARY_NAME" | awk '{print $5}')"
 
+# Checked BEFORE the install, for the same reason the smokes are: an unidentifiable
+# binary compiles perfectly and reads clean in the log, so installing first would put
+# it in front of live sessions and only then tell us it cannot be traced to a commit.
+echo "==> Checking provenance..."
+buildinfo="$(go version -m "$BINARY_NAME")"
+vcs_revision="$(printf '%s\n' "$buildinfo" | awk -F= '$1 ~ /[[:space:]]vcs\.revision$/ {{print $2}}')"
+vcs_modified="$(printf '%s\n' "$buildinfo" | awk -F= '$1 ~ /[[:space:]]vcs\.modified$/ {{print $2}}')"
+if [ -z "$vcs_revision" ]; then
+    echo "    REFUSING TO INSTALL: this binary carries no vcs.revision, so nothing can tie" >&2
+    echo "    it back to a commit. 'go build' writes no VCS stamp when it cannot find a .git" >&2
+    echo "    DIRECTORY, and it does not fail when that happens -- not even with -buildvcs=true." >&2
+    echo "    The usual cause is building from a git worktree, whose .git is a pointer file." >&2
+    echo "    Build from a real clone or checkout instead." >&2
+    exit 1
+fi
+echo "    vcs.revision=$vcs_revision"
+if [ "$vcs_modified" = "true" ]; then
+    echo "    WARNING: built from a DIRTY tree (vcs.modified=true). $vcs_revision names the" >&2
+    echo "    commit this binary was built NEAR, not the source it was built FROM, and that" >&2
+    echo "    source is not recoverable from any commit. Commit first for a reproducible build." >&2
+fi
+
 # Gate on vet, not `go test ./...`. TestYouTubeImportAPI calls the live YouTube
 # API and fails with "rate limited" on a perfectly good tree; a deploy gate that
 # goes red for reasons unrelated to the deploy is one people learn to ignore. vet

kayushkin/noteboard

deploy.sh
@@ -68,6 +68,27 @@ make test
 STAGED="$REPO_DIR/bin/noteboard"
 make build
 [ -x "$STAGED" ] || fail "make build produced no binary at $STAGED"
+
+# Checked BEFORE the install, for the same reason the smokes are: an unidentifiable
+# binary compiles perfectly and reads clean in the log, so installing first would put
+# it in front of live sessions and only then tell us it cannot be traced to a commit.
+echo "==> Checking provenance..."
+buildinfo="$(go version -m "$STAGED")"
+vcs_revision="$(printf '%s\n' "$buildinfo" | awk -F= '$1 ~ /[[:space:]]vcs\.revision$/ {{print $2}}')"
+vcs_modified="$(printf '%s\n' "$buildinfo" | awk -F= '$1 ~ /[[:space:]]vcs\.modified$/ {{print $2}}')"
+if [ -z "$vcs_revision" ]; then
+    echo "    'go build' writes no VCS stamp when it cannot find a .git DIRECTORY, and it does" >&2
+    echo "    not fail when that happens -- not even with -buildvcs=true. The usual cause is" >&2
+    echo "    building from a git worktree, whose .git is a pointer file. Build from a real" >&2
+    echo "    clone or checkout instead." >&2
+    fail "refusing to install "$STAGED": no vcs.revision, so nothing ties it back to a commit"
+fi
+echo "    vcs.revision=$vcs_revision"
+if [ "$vcs_modified" = "true" ]; then
+    echo "    WARNING: built from a DIRTY tree (vcs.modified=true). $vcs_revision names the" >&2
+    echo "    commit this binary was built NEAR, not the source it was built FROM, and that" >&2
+    echo "    source is not recoverable from any commit. Commit first for a reproducible build." >&2
+fi
 echo "    built: $(ls -lh "$STAGED" | awk '{print $5}')"
 
 step "boot-and-answer smoke on a throwaway DB, before touching the live one"